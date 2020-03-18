1f05dd9d89802bd26a68c855d9042a000071b688
vahernandez <valentin.hdz@gmail.com>
2020-03-17 21:48:27 -0600

    creacion del archivo variables.py
    Se crea arvhivo variables.py que corresponde al curso

---
+++ variables.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 16:37:58 2019
+
+@author: Cristyan Gil
+"""
+
+
+###    Variables    ####
+saludo = "Hola Mundo"
+n = 522
+pi = 3.1416
+c = 6.32+45j
+condicion = True
+
+print(saludo)
+print(n)
+print(pi)
+print(c)
+print(condicion)
+
+###   Modificar Variables   ###
+n = 1290
+print(n)
+
+###   Nombres incorrectos de Variables   ###
+6h = 5
+uno% = 0.1
+break = "romper"
+
+###    Conocer el tipo de Variables    ####
+type(saludo)
+type(n)
+type(pi)
+type(c)
+type(condicion)
+
+
